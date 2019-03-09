50544c6a1603959ca298aec19af780574643eebd
iusikder <iusikder@yahoo.com>
2019-03-08 23:42:40 -0500

    Third Commit for this Project
    Change-Id: I55d8f3ed2d7fead9ee45f92f3a972f9b1fa525df
    Signed-off-by: iusikder <iusikder@yahoo.com>

---
+++ GitFreeCRM/src/main/java/com/qa/crm/base/BaseTest.java
@@ -0,0 +1,62 @@
+package com.qa.crm.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import com.qa.crm.util.TestUtil;
+import com.qa.crm.util.WebEventListener;
+
+
+public class BaseTest {    
+    public static WebDriver driver;
+    public static Properties prop;
+    public static EventFiringWebDriver e_driver;
+    public static WebEventListener eventListener;
+
+    
+    public BaseTest(){    //Constructor of BaseTest Class to Initialize the Properties file.
+        try{
+            prop = new Properties();
+            //FileInputStream ip = new FileInputStream("/Proj_FreeCRMTest/src/main/java/com/crm/config/config.properties");
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/com/crm/qa/config/config.properties");
+            prop.load(ip);
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void initialization(){
+        String browserName = prop.getProperty("browser");            
+        if(browserName.equals("chrome")){
+            System.setProperty("webdriver.chrome.driver", "C:/AllDrivers/LatestChrome/chromedriver.exe");
+            driver = new ChromeDriver();                
+        }
+        else if(browserName.equals("FF")){
+            System.setProperty("webdriver.gecko.driver", "C:/AllDrivers/geckoDriver/geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
+        
+        //WebDriver FiringEvent
+        e_driver= new EventFiringWebDriver(driver);
+        //Now Create object of EventListenerHandler to register it EventFiringWebDriver
+        eventListener = new WebEventListener();
+        e_driver.register(eventListener);
+        driver = e_driver;            
+        
+        
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+    }
+
+    }
--- GitFreeCRM/src/main/java/com/qa/crm/pages/LoginPage.java
+++ GitFreeCRM/src/main/java/com/qa/crm/pages/LoginPage.java
@@ -1,5 +1,67 @@
 package com.qa.crm.pages;
 
-public class LoginPage {
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.qa.crm.base.BaseTest;
+import com.qa.crm.util.TestUtil;
+
+public class LoginPage extends BaseTest {
+    TestUtil testUtil;/////////
+    
+    //Page Factory/OR
+    @FindBy(name="username")
+    public WebElement username;
+    
+    @FindBy(name="password")
+    public WebElement password;
+    
+    //@FindBy(xpath="//input[@type='submit']")
+    @FindBy(xpath="//input[@class='btn btn-small' and @type='submit']")
+    WebElement loginBtn;
+    
+    @FindBy(xpath="//img[@class='img-responsive']")
+    WebElement crmLogo;
+    
+    //Initializing the Page Objects
+    public LoginPage(){
+        PageFactory.initElements(driver, this);
+    }
+    
+    
+    
+    //Methods
+    public String validateLoginPageTitle(){
+        return driver.getTitle();
+    }
+    
+    public boolean validateCRMImage(){
+        return crmLogo.isDisplayed();        
+    }
+    
+    //public HomePage login(String uName, String pWord) throws InterruptedException{
+    public HomePage login(String uName, String pWord) throws Exception{
+        TestUtil testUtil = new TestUtil();////// I'm just checking
+        username.sendKeys(uName);
+        password.sendKeys(pWord);        
+        
+        //Thread.sleep(4000);
+        //loginBtn.click();
+        testUtil.wait(4);
+    /*    Actions actions = new Actions(driver);
+        actions.click(loginBtn).perform();*/
+        
+        //Solution 2
+                //THIS JAVASCRIPT WORKED for the Radio Button also !!!!!
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("var evt = document.createEvent('MouseEvents');" + "evt.initMouseEvent('click',true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0,null);" + "arguments[0].dispatchEvent(evt);",(loginBtn));
+        
+            return new HomePage();  //After Login it will return HomePage Object.
+    }
+        
+    
 }
+
+
